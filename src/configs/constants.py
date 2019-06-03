0ad5e8ca252840fe1c690dd736ebc8f0eac352f7
lemonwaffle <ang.yizhe@u.nus.edu>
2019-06-03 22:03:36 +0800

    :D

--- src/configs/constants.py
+++ src/configs/constants.py
@@ -12,10 +12,10 @@ CLASS_COL = 'class'
 N_CLASSES = 196
 
 MODEL_DIR = 'saved'
-# MODEL_CSV_DIR = os.path.join(MODEL_DIR, 'model_csv')
-MODEL_CSV_DIR = MODEL_DIR
-# MODEL_LOGS_DIR = os.path.join(MODEL_DIR, 'model_logs')
-MODEL_LOGS_DIR = MODEL_DIR
-# MODEL_WEIGHTS_DIR = os.path.join(MODEL_DIR, 'model_weights')
-MODEL_WEIGHTS_DIR = MODEL_DIR
+MODEL_CSV_DIR = os.path.join(MODEL_DIR, 'model_csv')
+# MODEL_CSV_DIR = MODEL_DIR
+MODEL_LOGS_DIR = os.path.join(MODEL_DIR, 'model_logs')
+# MODEL_LOGS_DIR = MODEL_DIR
+MODEL_WEIGHTS_DIR = os.path.join(MODEL_DIR, 'model_weights')
+# MODEL_WEIGHTS_DIR = MODEL_DIR
 EXPS_PATH = os.path.join(MODEL_DIR, 'exps_info.csv')
